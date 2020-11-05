73da978b66492a2b84ccc6a43fa9610c20e5485f
Kyle Scumacher <kschumacher001@gmail.com>
2020-11-05 11:12:01 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        private double balance = 0;
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance += value;
+            }
+        }
+
+        public void Deposit(double i)
+        {
+            balance += i;
+        }
+        public void Withdraw(double i)
+        {
+            balance -= i;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool anotherTransaction;
+            var account = new BankAccount();
+
+            do
+            {
+                Console.WriteLine("Welcome to Broke People Bank!");
+                Console.WriteLine("Would you like to make a deposit or withdraw?");
+                var userResponse = Console.ReadLine().ToLower();
+                Console.Clear();
+
+
+                if (userResponse == "deposit")
+                {
+                    Console.WriteLine("Enter deposit amount.");
+                    var userDeposit = double.Parse(Console.ReadLine());
+                    account.Deposit(userDeposit);
+                }
+                if (userResponse == "withdraw")
+                {
+                    Console.WriteLine("Enter withdraw amount.");
+                    var userWithdraw = double.Parse(Console.ReadLine());
+                    account.Withdraw(userWithdraw);
+                }
+
+                Console.WriteLine($"Your balance is: {account.GetBalance()}");
+                PressEnter();
+
+                Console.WriteLine("Would you like to complete another transaction?");
+                var transactionResponse = Console.ReadLine().ToLower();
+                
+                if (transactionResponse == "yes")
+                {
+                    anotherTransaction = true;
+                    Console.Clear();
+                } 
+                else 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thank you, have a nice day.");
+                    anotherTransaction = false;
+                }
+
+
+
+            } while (anotherTransaction == true);
+            
+
+
+        }
+
+        private static void PressEnter()
+        {
+            Console.WriteLine("Press enter to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
